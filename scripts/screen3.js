2d30da7c7df11aa0251c640f29cc205f4c06ee88
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-08-20 12:11:11 +0530

    Allowed for titles to be changed

--- scripts/screen3.js
+++ scripts/screen3.js
@@ -1,3 +1,12 @@
+var musics = {
+	Rain: "rain.mp3",
+	River: "river.mp3",
+	Nature: "nature.mp3",
+	Night: "night.mp3",
+	Piano: "piano.mp3",
+};
+var selections = Object.keys(musics).randomize();
+
 // Changing the progress bar behind color on change
 // https://stackoverflow.com/questions/43771735/style-input-range-background-before-thumb
 document.querySelectorAll(".bar").forEach(function (el) {
@@ -8,3 +17,25 @@ document.querySelectorAll(".bar").forEach(function (el) {
 	};
 	el.oninput();
 });
+
+function get_current() {
+	return selections[2]; // the middle one
+}
+
+function update_title_display() {
+	document.querySelectorAll(".titles span").forEach(function (el, index) {
+		el.textContent = selections[index];
+	});
+}
+
+document.querySelectorAll(".titles span").forEach(function (el) {
+	el.addEventListener("click", function (event) {
+		event.preventDefault();
+		console.log(event.target.textContent);
+		while (selections[2] != el.textContent) {
+			selections = selections.rotate();
+		}
+		update_title_display();
+	});
+});
+update_title_display();
